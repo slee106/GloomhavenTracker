d9c03c9eb9287a81c0025217e004465e4c028cfb
slee106 <s.p.lee@hotmail.co.uk>
2020-05-07 11:44:15 +0100

    remove creds

--- src/Program.cs
+++ src/Program.cs
@@ -15,38 +15,7 @@ namespace GloomhavenTracker
     {
         public static void Main(string[] args)
         {
-            //CreateDatabase();
             CreateHostBuilder(args).Build().Run();
-
-
-        }
-
-        private static void CreateDatabase()
-        {
-            var connectionString = "Server=gloomhaven.cjpowrl0vm2n.eu-west-2.rds.amazonaws.com;Database=gloomhaven;user id=admin;password=admin123";
-
-            var upgrader =
-                DeployChanges.To
-                    .SqlDatabase(connectionString)
-                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
-                    .LogToConsole()
-                    .Build();
-
-            var result = upgrader.PerformUpgrade();
-
-            if (!result.Successful)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(result.Error);
-                Console.ResetColor();
-                Console.ReadLine();
-                return;
-            }
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Success!");
-            Console.ResetColor();
-            return;
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
